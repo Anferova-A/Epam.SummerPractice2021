5bce4f89ae2d2d1dda2cdcb0b953cce6a739876f
Anastasia Anferova <anf0404@bk.ru>
2021-07-11 18:56:03 +0400

    Сonnection string is placed in the configuration file

--- Epam.Shops/Epam.Shops.DAL/ShopsDB.cs
+++ Epam.Shops/Epam.Shops.DAL/ShopsDB.cs
@@ -1,11 +1,12 @@
 ﻿using Epam.Shops.Entities;
+using System.Configuration;
 using System.Data.Entity;
 
 namespace Epam.Shops.DAL
 {
     public class ShopsDB : DbContext
     {
-        public ShopsDB() : base(@"Data Source=LAPTOP-BDF48BLN\SQLEXPRESS;Initial Catalog=ShopsDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+        public ShopsDB() : base(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString)
         {
 
         }
